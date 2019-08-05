b1701be1bd710f415e57253cf1995ad6d47f3860
Sujay <ajmac1086@administrators-MacBook-Pro-22.local>
2019-08-05 13:07:21 +0530

    Singleton added

---
+++ SingletonPattern/singleton.js
@@ -0,0 +1,28 @@
+function calculateTip() {
+	var billAmt = document.getElementById("billamt").value;
+	var tipPercent = document.getElementById("tipParcentage").value;
+	var numOfPeople = document.getElementById("peopleamt").value;
+
+	if(billAmt=== "" || tipPercent==0) {
+		alert("Please enter valid input");
+		return;
+	}
+
+	if (numOfPeople === "" || numOfPeople <=1) {
+		numOfPeople = 1;
+		document.getElementById("each").innerHTML = "";
+	}
+	else {
+		document.getElementById("each").innerHTML = "each";
+	}
+	
+	var tipByEach =  (billAmt*tipPercent)/numOfPeople;
+	tipByEach = Math.round(tipByEach*100)/100;
+	tipByEach = tipByEach.toFixed(2);
+	document.getElementById("tip").innerHTML = tipByEach;
+}
+
+
+document.getElementById("calculate").onclick = function () {
+	calculateTip();
+};
